653d5824549729059823bd92964f70b82b694c5c
srisiva27 <srisiva27@gmail.com>
2020-11-17 01:02:42 +0530

    Source files for boom beach game production.

---
+++ BoomBeachProduction/Assets/BattleGame/Scripts/OtherGameData/GrassData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Assets.Scripts.OtherGameData
+{
+	[Serializable]
+	public class GrassData:ScriptableObject 
+	{
+		public string GrassCategoryId;
+		[FormerlySerializedAs("GrassCategory")]
+		public List<GameObject> Category;
+		
+	}
+}
---
+++ BoomBeachProduction/Assets/BattleGame/Scripts/UIControllersAndData/Player/LevelData.cs
@@ -0,0 +1,20 @@
+namespace Assets.Scripts.UIControllersAndData.Player
+{
+	
+	public class LevelData 
+	{
+
+		
+		public LevelData() 
+		{
+		}
+
+		private int _level;
+
+		public int Level
+		{
+			get { return _level; }
+			set { _level = value; }
+		}
+	}
+}
